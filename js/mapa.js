c409e678ca66cc24eaffbdb07a28eefa72c9d75a
Slower007 <167630924+Slower007@users.noreply.github.com>
2025-05-13 17:11:36 -0600

    Delete js directory

--- js/mapa.js
+++
@@ -1,9 +0,0 @@
-import { sitios } from './sitios.js';
-export function renderMap(){
-  const coords = sitios.map(s => `${s.latitude},${s.longitude}`).join('|');
-  document.getElementById('view-mapa').innerHTML = `
-    <iframe 
-      src="https://maps.google.com/maps?q=${coords}&z=13&output=embed"
-      frameborder="0" allowfullscreen>
-    </iframe>`;
-}
