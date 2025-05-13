2335c9abed947c0ebb06b176fbbfd4f8b0c0dfe1
Slower007 <167630924+Slower007@users.noreply.github.com>
2025-05-13 17:19:11 -0600

    Delete App Turística Modular B directory

--- App Turística Modular B/js/mapa.js
+++
@@ -1,9 +0,0 @@
-import { sitios } from './sitios.js';
-export function renderMap(){
-  const markers = sitios.map(s => s.latitude + ',' + s.longitude).join('|');
-  document.getElementById('view-mapa').innerHTML = `
-    <iframe 
-      src="https://maps.google.com/maps?q=${markers}&z=13&output=embed" 
-      frameborder="0" allowfullscreen>
-    </iframe>`;
-}
